5b78db7cc934234180c51fa5e85ff0431bd66064
Goutam Sachdeva <goutam.sach@gmail.com>
2023-12-31 17:35:34 +0530

    full

--- src/utils/context.js
+++ src/utils/context.js
@@ -1,10 +1,23 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 export const Context = createContext();
 
 const AppContext = ({ children }) => {
   const [categories, setCategories] = useState();
   const [products, setproducts] = useState();
+  // const [showCart, setShowCart] = useState(false);
+  // const [cartItems, setCartItems] = useState([]);
+  // const [cartCount, setCartCount] = useState(0);
+  // const [cartSubTotal, setCartSubTotal] = useState(0);
+  // const location = useLocation();
+
+  // useEffect(() => {}, [cartItems]);
+
+  // const handleAddToCart = () => { let items = [...cartItems]; let index= items.};
+  // const handleRemoveFromCart = () => {};
+  // const handleCartQty = () => {};
+
   return (
     <Context.Provider
       value={{
@@ -12,6 +25,17 @@ const AppContext = ({ children }) => {
         setCategories,
         products,
         setproducts,
+        // showCart,
+        // setShowCart,
+        // cartItems,
+        // setCartItems,
+        // cartCount,
+        // setCartCount,
+        // cartSubTotal,
+        // setCartSubTotal,
+        // handleAddToCart,
+        // handleRemoveFromCart,
+        // handleCartQty,
       }}
     >
       {children}
